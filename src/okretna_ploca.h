7f850124186e118734464056ba488af0b4eaaf61
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-02 23:18:52 +0200

    Update okretna_ploca.h

--- src/okretna_ploca.h
+++ src/okretna_ploca.h
@@ -3,3 +3,9 @@
 
 void inicijalizirajPlocu();
 void upravljajPločom();
+
+// Ručno postavljanje trenutnog položaja ploče (0-95)
+void postaviTrenutniPolozajPloce(int pozicija);
+
+// Kompenzacija položaja ploče nakon nestanka struje
+void kompenzirajPlocu();
