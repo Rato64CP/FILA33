ef5a63ef857fb23ce98f34ec97dc22c7cda3ebe7
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-03 00:18:54 +0200

    Create synchronizacija.cpp

---
+++ src/synchronizacija.cpp
@@ -0,0 +1,18 @@
+// synchronizacija.cpp
+#include <RTClib.h>
+#include <EEPROM.h>
+#include "kazaljke_sata.h"
+#include "okretna_ploca.h"
+#include "vrijeme_izvor.h"
+
+void sinkronizirajVrijemeIzvora(const DateTime& novoVrijeme, IzvorVremena izvor) {
+  RTC_DS3231 rtc;
+  rtc.adjust(novoVrijeme);
+
+  // Spremi izvor
+  EEPROM.put(0, (int)izvor);
+
+  // Pametna kompenzacija kazaljki i ploce
+  kompenzirajKazaljke(true);
+  kompenzirajPlocu(true);
+}
