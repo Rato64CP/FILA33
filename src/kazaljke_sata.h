c925d6d8953719ad5b4e3032581d32050a42ac47
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-02 23:16:25 +0200

    Update kazaljke_sata.h

--- src/kazaljke_sata.h
+++ src/kazaljke_sata.h
@@ -1,6 +1,17 @@
 // kazaljke_sata.h
 #pragma once
 
+// Inicijalizacija pinova za releje kazaljki
 void inicijalizirajKazaljke();
+
+// Poziv iz loop() svakih 1000ms za pomicanje kazaljki
 void upravljajKazaljkama();
-void pomakniKazaljkeNaMinutu(int ukupnoMinuta);
+
+// Ručno postavljanje kazaljki - korisnik unosi koliko je trenutno pokazano
+void postaviTrenutniPolozajKazaljki(int trenutnaMinuta);
+
+// Ručno pomicanje kazaljki da bi se dosegla prava minuta
+void pomakniKazaljkeNaMinutu(int ciljMinuta);
+
+// Kompenzacija nakon nestanka struje - vraćanje u sinkronizaciju
+void kompenzirajKazaljke();
