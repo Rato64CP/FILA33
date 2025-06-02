789702240975f94509e1c0fd4bb79998be075a8f
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-03 00:06:03 +0200

    Update okretna_ploca.cpp

--- src/okretna_ploca.cpp
+++ src/okretna_ploca.cpp
@@ -1,27 +1,28 @@
 // okretna_ploca.cpp
-#include "okretna_ploca.h"
 #include <Arduino.h>
-#include <EEPROM.h>
 #include <RTClib.h>
+#include <EEPROM.h>
+#include "okretna_ploca.h"
+#include "podesavanja_piny.h"
 
-const int relejPloca1 = 12;
-const int relejPloca2 = 13;
-const unsigned long TRAJANJE_IMPULSA_PLOCE = 6000UL;
-int memoriranaPozicijaPloce = 0;
-int offsetMinuta = 14; // podesivo u postavkama, 0–15
+const unsigned long POLA_OKRETA_MS = 6000UL;
 
 static unsigned long vrijemeStarta = 0;
 static bool uTijeku = false;
 static bool drugaFaza = false;
 static DateTime prosloVrijeme;
 
+int pozicijaPloce = 0; // 0-63
+int offsetMinuta = 14; // podesivo u postavkama, 0–15
+
 void inicijalizirajPlocu() {
-  pinMode(relejPloca1, OUTPUT);
-  pinMode(relejPloca2, OUTPUT);
-  digitalWrite(relejPloca1, LOW);
-  digitalWrite(relejPloca2, LOW);
-  EEPROM.get(20, memoriranaPozicijaPloce);
-  if (memoriranaPozicijaPloce < 0 || memoriranaPozicijaPloce > 63) memoriranaPozicijaPloce = 0;
+  pinMode(PIN_RELEJ_PARNE_PLOCE, OUTPUT);
+  pinMode(PIN_RELEJ_NEPARNE_PLOCE, OUTPUT);
+  digitalWrite(PIN_RELEJ_PARNE_PLOCE, LOW);
+  digitalWrite(PIN_RELEJ_NEPARNE_PLOCE, LOW);
+
+  EEPROM.get(20, pozicijaPloce);
+  if (pozicijaPloce < 0 || pozicijaPloce > 63) pozicijaPloce = 0;
   EEPROM.get(22, offsetMinuta);
   if (offsetMinuta < 0 || offsetMinuta > 15) offsetMinuta = 14;
 
@@ -39,7 +40,7 @@ void upravljajPločom() {
   bool okidac = (minuta % 15 == offsetMinuta) && sekunda == 0;
   if (okidac && !uTijeku && now != prosloVrijeme) {
     prosloVrijeme = now;
-    digitalWrite(relejPloca1, HIGH);
+    digitalWrite(PIN_RELEJ_PARNE_PLOCE, HIGH);
     vrijemeStarta = millis();
     uTijeku = true;
     drugaFaza = false;
@@ -48,25 +49,25 @@ void upravljajPločom() {
   if (uTijeku) {
     unsigned long trajanje = millis() - vrijemeStarta;
 
-    if (!drugaFaza && trajanje >= TRAJANJE_IMPULSA_PLOCE) {
-      digitalWrite(relejPloca1, LOW);
-      digitalWrite(relejPloca2, HIGH);
+    if (!drugaFaza && trajanje >= POLA_OKRETA_MS) {
+      digitalWrite(PIN_RELEJ_PARNE_PLOCE, LOW);
+      digitalWrite(PIN_RELEJ_NEPARNE_PLOCE, HIGH);
       drugaFaza = true;
     }
 
-    if (trajanje >= 2 * TRAJANJE_IMPULSA_PLOCE) {
-      digitalWrite(relejPloca2, LOW);
+    if (trajanje >= 2 * POLA_OKRETA_MS) {
+      digitalWrite(PIN_RELEJ_NEPARNE_PLOCE, LOW);
       uTijeku = false;
       drugaFaza = false;
-      memoriranaPozicijaPloce = (memoriranaPozicijaPloce + 1) % 64;
-      EEPROM.put(20, memoriranaPozicijaPloce);
+      pozicijaPloce = (pozicijaPloce + 1) % 64;
+      EEPROM.put(20, pozicijaPloce);
     }
   }
 }
 
 void postaviTrenutniPolozajPloce(int pozicija) {
-  memoriranaPozicijaPloce = constrain(pozicija, 0, 63);
-  EEPROM.put(20, memoriranaPozicijaPloce);
+  pozicijaPloce = constrain(pozicija, 0, 63);
+  EEPROM.put(20, pozicijaPloce);
 }
 
 void postaviOffsetMinuta(int offset) {
@@ -74,29 +75,12 @@ void postaviOffsetMinuta(int offset) {
   EEPROM.put(22, offsetMinuta);
 }
 
-int dohvatiTrenutnuPozicijuPloce() {
-  return memoriranaPozicijaPloce;
-}
-
 int dohvatiOffsetMinuta() {
   return offsetMinuta;
 }
 
-void pomakniPlocuZa(int brojKvadranata) {
-  for (int i = 0; i < brojKvadranata; ++i) {
-    digitalWrite(relejPloca1, HIGH);
-    delay(TRAJANJE_IMPULSA_PLOCE);
-    digitalWrite(relejPloca1, LOW);
-    delay(200);
-    digitalWrite(relejPloca2, HIGH);
-    delay(TRAJANJE_IMPULSA_PLOCE);
-    digitalWrite(relejPloca2, LOW);
-    delay(500);
-
-    memoriranaPozicijaPloce++;
-    if (memoriranaPozicijaPloce > 63) memoriranaPozicijaPloce = 0;
-  }
-  EEPROM.put(20, memoriranaPozicijaPloce);
+int dohvatiPozicijuPloce() {
+  return pozicijaPloce;
 }
 
 void kompenzirajPlocu() {
@@ -106,8 +90,19 @@ void kompenzirajPlocu() {
   if (sati < 5 || sati > 20) return;
 
   int ciljPozicija = (sati - 5) * 4 + (minuta / 15);
-  int razlika = ciljPozicija - memoriranaPozicijaPloce;
+  int razlika = ciljPozicija - pozicijaPloce;
   if (razlika < 0) razlika += 64;
 
-  pomakniPlocuZa(razlika);
+  for (int i = 0; i < razlika; i++) {
+    digitalWrite(PIN_RELEJ_PARNE_PLOCE, HIGH);
+    delay(POLA_OKRETA_MS);
+    digitalWrite(PIN_RELEJ_PARNE_PLOCE, LOW);
+    delay(200);
+    digitalWrite(PIN_RELEJ_NEPARNE_PLOCE, HIGH);
+    delay(POLA_OKRETA_MS);
+    digitalWrite(PIN_RELEJ_NEPARNE_PLOCE, LOW);
+    delay(500);
+  }
+  pozicijaPloce = ciljPozicija;
+  EEPROM.put(20, pozicijaPloce);
 }
