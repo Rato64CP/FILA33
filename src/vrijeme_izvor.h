9aa581dc0d099e558fb0a12cddfad65d4aab8e35
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-03 00:24:26 +0200

    Create vrijeme_izvor.h

---
+++ src/vrijeme_izvor.h
@@ -0,0 +1,19 @@
+// vrijeme_izvor.h
+#ifndef VRIJEME_IZVOR_H
+#define VRIJEME_IZVOR_H
+
+#include <RTClib.h>
+
+enum IzvorVremena {
+  RTC_VRIJEME = 0,
+  NTP_VRIJEME = 1,
+  DCF_VRIJEME = 2,
+  NEPOZNATO_VRIJEME = 3
+};
+
+void setZadnjaSinkronizacija(IzvorVremena izvor, const DateTime& vrijeme);
+IzvorVremena getZadnjiIzvor();
+DateTime getZadnjeSinkroniziranoVrijeme();
+bool jeSinkronizacijaZastarjela();
+
+#endif
