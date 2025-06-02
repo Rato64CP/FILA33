fa0a4b3e7b17e077d9622982e6e55783cb6d10b8
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-03 00:25:15 +0200

    Create vrijeme_izvor.cpp

---
+++ src/vrijeme_izvor.cpp
@@ -0,0 +1,34 @@
+// vrijeme_izvor.cpp
+#include <EEPROM.h>
+#include <RTClib.h>
+#include "vrijeme_izvor.h"
+
+#define EEPROM_ADRESA_IZVOR 0
+#define EEPROM_ADRESA_VRIJEME 4
+
+void setZadnjaSinkronizacija(IzvorVremena izvor, const DateTime& vrijeme) {
+  EEPROM.put(EEPROM_ADRESA_IZVOR, (int)izvor);
+  uint32_t ts = vrijeme.unixtime();
+  EEPROM.put(EEPROM_ADRESA_VRIJEME, ts);
+}
+
+IzvorVremena getZadnjiIzvor() {
+  int i;
+  EEPROM.get(EEPROM_ADRESA_IZVOR, i);
+  if (i < 0 || i > 3) return NEPOZNATO_VRIJEME;
+  return (IzvorVremena)i;
+}
+
+DateTime getZadnjeSinkroniziranoVrijeme() {
+  uint32_t ts;
+  EEPROM.get(EEPROM_ADRESA_VRIJEME, ts);
+  return DateTime(ts);
+}
+
+bool jeSinkronizacijaZastarjela() {
+  RTC_DS3231 rtc;
+  DateTime sad = rtc.now();
+  DateTime zadnje = getZadnjeSinkroniziranoVrijeme();
+  TimeSpan razlika = sad - zadnje;
+  return razlika.totalseconds() > 86400; // 24h
+}
