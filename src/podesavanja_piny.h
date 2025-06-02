d2b3b82a5293dbe7b28e9fa196b112c23491db8c
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-02 23:59:05 +0200

    Create podesavanja_piny.h

---
+++ src/podesavanja_piny.h
@@ -0,0 +1,12 @@
+// podesavanja_piny.h
+#pragma once
+
+// Kazaljke sata
+#define PIN_RELEJ_PARNE_KAZALJKE   10
+#define PIN_RELEJ_NEPARNE_KAZALJKE 11
+
+// Okretna ploča
+#define PIN_RELEJ_PARNE_PLOCE      8
+#define PIN_RELEJ_NEPARNE_PLOCE    9
+
+// Dodaj ostale pinove prema potrebi...
