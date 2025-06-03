b68bacb27db1ec44f9fab333ff7b81420cbebc48
Rato64CP <ratomir.vukorepa@gmail.com>
2025-06-04 00:49:47 +0200

    Delete src/rtc_vrijeme.h

--- src/rtc_vrijeme.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <RTClib.h>
-
-bool isDST(int dan, int mjesec, int danUTjednu); // provjera DST razdoblja
-void syncNTP();                 // sinkronizacija RTC-a preko NTP-a (ESP modul)
-void syncDCF();                 // sinkronizacija RTC-a prema DCF77 signalu
-extern String izvorVremena;     // "NTP", "DCF", "RU" ili "RTC" – zadnji izvor
